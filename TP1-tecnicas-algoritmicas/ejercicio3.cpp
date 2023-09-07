875dbaab2c9169e85f4521a17d6f74e843778628
matuneville <nevillematias@gmail.com>
2023-09-07 10:25:59 -0300

    corrijo archivos

--- TP1-tecnicas-algoritmicas/ejercicio3.cpp
+++ TP1-tecnicas-algoritmicas/ejercicio3.cpp
@@ -1,93 +1,102 @@
 #include <iostream>
 #include "vector"
-
+#include <algorithm>
 using namespace std;
 
-int precio = 0;
-
-vector<int> valores = {};
-vector<char> solActual = {};
-
-
-void comparoSignos(vector<char> s1){
-    if(solActual.empty())
-        solActual = s1;
-    else{
-        for (int i = 0; i < s1.size(); i++) {
-            if (s1[i] != solActual[i] && (s1[i] != '?' or solActual[i] != '?')) {
-                solActual[i] = '?';
-            }
-        }
-    }
-}
+vector<pair<int,int>> valoresG={};
+int precioPoda=0;
+int sumaRestante=0;
 
 bool IMPOSIBOL(int sumaActual, int sumaRestante){
     bool res=false;
-    if(sumaRestante>0){
-        if(sumaActual-sumaRestante > precio)
+    if(sumaActual==0) return res;
+
+    else if(sumaActual<0){
+        if(sumaActual+sumaRestante<0){
             res=true;
-    }
-    else{
-        if (sumaActual+sumaRestante < precio)
+        }
+    } else{
+        if(sumaActual-sumaRestante>0){
             res=true;
+        }
     }
     return res;
 }
 
-void AFIP(int i, vector<char> resActual, int sumaActual, int sumaRestante){
-    if(i == valores.size()){
-        if(sumaActual == precio){
-            comparoSignos(resActual);
-        }
+bool   puedoMas(vector<pair<int,int>>& valores, int precioActual, int iBorrar,int indice, int sumaRestante){
+    if(precioActual == 0 && indice == valores.size()) {
+        return true;
     }
-
-    else if(IMPOSIBOL(sumaActual, sumaRestante)) return;
-    else if (sumaActual + sumaRestante == precio){
-        for(int j = i; j < valores.size(); j++){
-            resActual[j] = '+';
-        }
-        AFIP(valores.size(), resActual, precio, 0);
+    else if(precioActual != 0 && indice == valores.size()) {
+        return false;
     }
-    else if(sumaActual - sumaRestante == precio){
-        for(int j = i; j < valores.size(); j++){
-            resActual[j] = '-';
-        }
-        AFIP(valores.size(), resActual, precio, 0);
+    else if(IMPOSIBOL(precioActual, sumaRestante) && valores[indice].first!=0){
+        return false;
+    }
+    if(indice != iBorrar){
+        return puedoMas(valores, precioActual + valores[indice].first,iBorrar,indice+1,sumaRestante-valores[indice].first) or puedoMas(valores, precioActual - valores[indice].first,iBorrar,indice+1,sumaRestante-valores[indice].first);
     }
     else{
-        resActual[i] = '+';
-        AFIP(i+1, resActual, sumaActual + valores[i], sumaRestante - valores[i]);
-        resActual[i] = '-';
-        AFIP(i+1, resActual, sumaActual - valores[i], sumaRestante - valores[i]);
+        return puedoMas(valores, precioActual ,iBorrar,indice+1,sumaRestante) or puedoMas(valores, precioActual ,iBorrar,indice+1,sumaRestante);
     }
+}
+
+
+
+vector<char> AFIP(int precio){
+    vector<pair<char,int>> res (valoresG.size(), make_pair('A',0));
+    for(int i = 0; i < valoresG.size(); i++){
+        bool mas = puedoMas(valoresG, precio - valoresG[i].first, i, 0, sumaRestante-valoresG[i].first);
+        bool menos = puedoMas(valoresG, precio + valoresG[i].first, i, 0,sumaRestante-valoresG[i].first);
+        if(mas && menos)
+            res[i] = make_pair('?',valoresG[i].second);
+        else if(mas)
+            res[i] = make_pair('+',valoresG[i].second);
+        else
+            res[i] = make_pair('-',valoresG[i].second);
+    }
+    sort(res.begin(), res.end(), [](const std::pair<int, int>& a, const std::pair<int, int>& b) {
+        return a.second < b.second;
+    });
+    vector<char> result (valoresG.size(), 'A');
+    for(int i=0;i<res.size();i++){
+        result[i]=res[i].first;
+    }
+    return result;
 }
 
 int main(){
     int tests;
     cin >> tests;
     vector<vector<char>> results;
+    int precio=0;
     for (int i = 0; i < tests; i++){
-        
+        precio=0;
         int cantValores, precioActual;
         cin >> cantValores >> precioActual;
-        vector<int> nuevoValores(cantValores, 0);
-        valores = nuevoValores;
+        vector<pair<int,int>> nuevoValores(cantValores, make_pair(0,0));
+        valoresG = nuevoValores;
 
         precio = precioActual;
         for(int j = 0; j < cantValores; j++){
             int valor=0;
             cin >> valor;
-            valores[j] = valor;
+            valoresG[j] = make_pair(valor,j);
         }
         int sumaTotal = 0;
-        for(int n: valores) sumaTotal += n;
+        for(pair<int,int> n: valoresG) sumaTotal += n.first;
 
-        vector<char> resActual(valores.size(),'A');
+        vector<char> resActual(valoresG.size(),'A');
 
-        AFIP(0,resActual,0,sumaTotal);
+        precioPoda=precio;
+        sumaRestante=sumaTotal;
 
-        results.push_back(solActual);
-        solActual = {};
+        sort(valoresG.begin(), valoresG.end(), [](const std::pair<int, int>& a, const std::pair<int, int>& b) {
+            return a.first > b.first;
+        });
+
+        results.push_back(AFIP(precio));
+        sumaTotal = 0;
     }
     for(int i = 0; i < tests;i++){
         for (int j = 0; j < results[i].size(); j++) {
@@ -95,4 +104,5 @@ int main(){
         }
         if(i != tests-1) cout << endl;
     }
+    return 0;
 }
