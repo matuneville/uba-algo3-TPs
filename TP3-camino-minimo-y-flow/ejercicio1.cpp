abff93ad77ab7c79c92cc7dd6361187711024288
matuneville <nevillematias@gmail.com>
2023-10-29 10:00:03 -0300

    ej1 comentado

--- TP3-camino-minimo-y-flow/ejercicio1.cpp
+++ TP3-camino-minimo-y-flow/ejercicio1.cpp
@@ -6,9 +6,8 @@ int N;
 int P;
 int inf=1e9;
 vector<int> dist;
-
-
 vector<vector<pair<int,int>>> adj;
+
 
 void relax(int u, int v, int w){
     if (dist[v] > dist[u]+w){
@@ -16,7 +15,7 @@ void relax(int u, int v, int w){
     }
 }
 
-void dagShortPaths(){
+void DAGshortestPath(){ // buscar shortest path en DAG, algoritmo de la biblia (Cormen's Introduction to Algorithms)
     for (int i = 0; i < 4*N; ++i) {
         for (auto v: adj[i]) {
             relax(i,v.first,v.second);
@@ -27,34 +26,18 @@ void dagShortPaths(){
 void armarListaAdj(){
     for(int j=0; j<N; j++){
         adj[j].push_back(make_pair(j+1,1)); // armo grafo de 4 niveles para que sólo pueda usar 3 pasadizos
-        adj[j+N].push_back(make_pair(j+N+1,1));
+        adj[j+N].push_back(make_pair(j+N+1,1)); // basicamente se repiten los N salones 4 veces
         adj[j+2*N].push_back(make_pair(j+2*N+1,1));
         adj[j+3*N].push_back(make_pair(j+3*N+1,1));
     }
     for(int p=1; p<=P; p++){
         int a, b;
         cin >> a >> b;
-        a--, b--;
+        a--, b--;    // los pasadizos van dirigidos de arriba hacia abajo
         adj[a].push_back(make_pair(b+N,2));
         adj[a+N].push_back(make_pair(b+2*N,2));
         adj[a+2*N].push_back(make_pair(b+3*N,2));
     }
-}
-
-int buscarMinimo(){
-    vector<int> candidatos;
-    candidatos.push_back(dist[N-1]);
-    candidatos.push_back(dist[2*N-1]);
-    candidatos.push_back(dist[3*N-1]);
-    candidatos.push_back(dist[4*N-1]);
-
-    int res=candidatos[0];
-    for(int i=1; i< candidatos.size(); i++){
-        if(candidatos[i]<res){
-            res=candidatos[i];
-        }
-    }
-    return res+1;
 }
 
 
@@ -67,13 +50,19 @@ int main() {
     for(int k=1; k<= tests; k++){
         cin >> N >> P;
         adj.assign(4*N,{});
-        armarListaAdj();
-        dist.assign(4*N,inf);
+        armarListaAdj();               // en esa función se explica cómo se arma la lista de adyacencia
+        dist.assign(4*N,inf);   // inicializo single-source distances
         dist[0]=0;
-        dagShortPaths();
+        DAGshortestPath();      // como ya queda topologicamente ordenado, puedo buscar el camino minimo
 
-        int res=buscarMinimo();
-        output.push_back(res);
+        int res = inf;
+        int final = N-1;
+        for (int i = 0; i < 4; ++i) { // busco el mínimo de los 4 finales obtenidos
+            if(dist[final] < res) res = dist[final];
+            final += N;
+        }
+
+        output.push_back(res+1); // +1 porque arranca en el primer salon con costo 1
     }
 
     for (int i = 0; i < tests; ++i) {
